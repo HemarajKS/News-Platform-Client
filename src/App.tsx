f544cbfb42501b3d1ae9ffcb6e5e3e1674581328
hemaraja.shettigar <hemaraja.shettigar@robosoftin.com>
2025-04-20 08:26:39 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./pages/Home";
 import Article from "./pages/Article";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 
 const App: React.FC = () => {
+  const location = useLocation();
+
+  //On route change scroll to top
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/article/:id" element={<Article />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 };
 
